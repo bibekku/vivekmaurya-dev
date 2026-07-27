322f6b5340af9885c9222cafd9cdfdfbe3164c7a
Vivek Maurya <vivekcmaurya30@gmail.com>
2026-07-27 17:51:50 +0530

    Added scaffolding

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import sitemap from '@astrojs/sitemap';
+
+import mdx from '@astrojs/mdx';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://vivekmaurya.dev',
+  integrations: [sitemap(), mdx(), react()],
+});
